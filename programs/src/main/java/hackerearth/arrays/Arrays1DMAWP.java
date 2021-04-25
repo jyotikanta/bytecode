234bf2ea4999dc3ff874dc6f880fda30ab261258
Jyotikanta <--global>
2021-04-25 18:13:35 +0530

    adding a comment and changing variable names

--- programs/src/main/java/hackerearth/arrays/Arrays1DMAWP.java
+++ programs/src/main/java/hackerearth/arrays/Arrays1DMAWP.java
@@ -1,27 +1,27 @@
 package hackerearth.arrays;
 
 import java.util.Scanner;
-
+//One dimensional array addition
 public class Arrays1DMAWP {
     public static void main(String args[]) throws Exception {
-        Scanner s = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        int m = s.nextInt();
+        int size = input.nextInt();
 
-        int[] array1 = new int[m];
-        int[] array2 = new int[m];
+        int[] array1 = new int[size];
+        int[] array2 = new int[size];
 
-        for(int i=0;i<m;i++)
+        for(int i=0;i<size;i++)
         {
-            array1[i] = s.nextInt();
+            array1[i] = input.nextInt();
         }
 
-        for(int i=0;i<m;i++)
+        for(int i=0;i<size;i++)
         {
-            array2[i] = s.nextInt();
+            array2[i] = input.nextInt();
         }
 
-        for(int i=0;i<m;i++)
+        for(int i=0;i<size;i++)
         {
             System.out.print(array1[i]+array2[i]+" ");
         }
