6e4969a89788f9c9f4b862ba56a998c42c58617b
jnayak <jyotikanta.nayak@microfocus.com>
2023-01-15 00:56:43 +0530

    GAv1

--- programs/src/main/java/neetcode/arrays/GroupAnagramV1.java
+++ programs/src/main/java/neetcode/arrays/GroupAnagramV1.java
@@ -1,2 +1,36 @@
-package neetcode.arrays;public class GroupAnagramV1 {
+package neetcode.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagramV1
+{
+    public static void main(String[] args)
+    {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> ga = groupAnagrams(strs);
+        System.out.println(ga);
+    }
+    public static List<List<String>> groupAnagrams(String[] strs)
+    {
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s : strs)
+        {
+            int[] count = new int[26];
+            for(char c : s.toCharArray())
+            {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            if(!map.containsKey(key))
+            {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
 }
