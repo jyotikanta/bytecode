f3cd12b85a3d39650823469d4ed9f46c09fc7f3e
Jnayak <Jyotikanta.Nayak@microfocus.com>
2023-04-09 12:03:13 +0530

    Adding new program

---
+++ programs/src/main/java/neetcode/arrays/TopKFreqElement.java
@@ -0,0 +1,43 @@
+package neetcode.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class TopKFreqElement
+{
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,2,3,3,3,4};
+        int topK = 1;
+        TopKFreqElement topKObj = new TopKFreqElement();
+        System.out.println(Arrays.toString(topKObj.topKFrequent(nums, topK)));
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> freqMap = new HashMap<>(nums.length);
+        for(int num: nums){
+            freqMap.put(num, freqMap.getOrDefault(num,0)+1);
+        }
+        //create buckets
+        ArrayList<ArrayList<Integer>> buckets = new ArrayList<>();
+        for(int i=0;i<=nums.length;i++){
+            buckets.add(new ArrayList<>());
+        }
+        for(int key: freqMap.keySet()){
+            int valueAtKey = freqMap.get(key);
+            buckets.get(valueAtKey).add(key);
+        }
+        List<Integer> topKList = new ArrayList<>();
+        for(int i=nums.length;i>=0 && topKList.size()<k; i--) {
+            topKList.addAll(buckets.get(i));
+        }
+        int[] topK = new int[topKList.size()];
+        int idx=0;
+        for(int i : topKList)
+        {
+            topK[idx++] =  i;
+        }
+        return topK;
+    }
+}
