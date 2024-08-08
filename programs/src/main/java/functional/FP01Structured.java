1137dd6a27869e4705332cca7386a84033516d15
Jyotikanta Nayak <jyotigml@gmail.com>
2024-08-09 01:18:45 +0530

    Adding new program for functional programming

---
+++ programs/src/main/java/functional/FP01Structured.java
@@ -0,0 +1,49 @@
+package functional;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FP01Structured {
+    public static void main(String[] args) {
+        System.out.println("=======================EVEN======================");
+        printAllEvenNumbersFromTheList(Arrays.asList(23,12,5,8,1,9,7,12,64,99));
+        System.out.println("=======================ODD=======================");
+        printAllOddNumbersFromTheList(Arrays.asList(23,12,5,8,1,9,7,12,64,99));
+        System.out.println("=======================CUBE======================");
+        printCubeOfAllEvenNumbersFromTheList(Arrays.asList(23,12,5,8,1,9,7,12,64,99));
+    }
+
+    private static void printCubeOfAllEvenNumbersFromTheList(List<Integer> list) {
+        list.stream()
+                .filter(FP01Structured::isOdd)
+                .map(n->n*n*n + " ")
+                .forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static void printAllOddNumbersFromTheList(List<Integer> list) {
+        list.stream()
+                .filter(FP01Structured::isOdd)
+                .map(n-> n +" ")
+                .forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static void printAllEvenNumbersFromTheList(List<Integer> list) {
+        list.stream()
+                .filter(FP01Structured::isEven)
+                .map(n-> n+" ")
+                .forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static boolean isOdd(int n){
+        return n%2!=0;
+    }
+
+    private static boolean isEven(int n){
+        return n%2==0;
+    }
+
+
+}
